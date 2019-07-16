cde396e889e1de7adbb8f85cfa3a1a128527e863
Abdul Moeed <abdulfastian1@gmail.com>
2019-07-16 15:45:59 +0500

    Pushing Sample Example of Naxmo SMS

---
+++ NexmoSMSApi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Nexmo.Api;
+using Nexmo.Api.ClientMethods;
+using NexmoSMSApi;
+namespace NexmoSMSApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Goind to send Text message using Nexmo");
+
+            var client = new Client(creds: new Nexmo.Api.Request.Credentials
+            {
+                ApiKey = "your-api-key",
+                ApiSecret = "your-api-secret"
+            });
+            var results = client.SMS.Send(request: new Nexmo.Api.SMS.SMSRequest
+            {
+                from = "Acme Inc",
+                to = "your-number",
+                text = "A test SMS sent using the SMS API MR MInaam"
+            });
+
+            if (results.messages.Count >= 1)
+            {
+                if (results.messages[0].status == "0")
+                    Console.WriteLine("Message sent successfully.");
+                else
+                    Console.WriteLine("Message failed with error:"+results.messages[0].error_text);
+            }
+
+        }
+    }
+}
